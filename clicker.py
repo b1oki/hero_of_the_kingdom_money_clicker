d571e8cfb4a5c93cf886a41e09a7114c78dff53d
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2020-05-10 04:50:11 +0500

    Feat: GUI clicker

--- clicker.py
+++ clicker.py
@@ -8,109 +8,114 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 
-class Clicker(object):
+class ClickerExecutor(object):
     def __init__(self, cash_value: int) -> None:
-        self.logger = logging.getLogger('clicker')
-        self.logger.addHandler(logging.StreamHandler())
+        self.log = logging.getLogger('clicker')
+        self.log.setLevel(logging.DEBUG)
+        self.log.addHandler(logging.StreamHandler())
         self.cash_value = cash_value
+        self.is_fullscreen = False
+        self.mouse = Controller()
 
-    def log(self, message: str) -> None:
-        self.logger.info(message)
+    def get_mouse_position(self):
+        position = self.mouse.position
+        self.log.debug('The current pointer position is {0}'.format(position))
+        return position
 
-def get_mouse_position(output=False):
-    mouse = Controller()
-    position = mouse.position
-    if output:
-        print('The current pointer position is {0}'.format(position))
-    return position
+    def mouse_click(self, x, y, x_offset=0, y_offset=0, delay=0.0):
+        need_position = (x + x_offset, y + y_offset)
+        self.log.debug('Move mouse to {} before click'.format(need_position))
+        self.mouse.position = need_position
+        self.get_mouse_position()
+        self.log.debug('Mouse click')
+        time.sleep(0.1)
+        self.mouse.press(Button.left)
+        time.sleep(0.1)
+        self.mouse.release(Button.left)
+        time.sleep(delay)
+        self.log.debug('Verify mouse after click')
+        if self.get_mouse_position() != need_position:
+            raise SystemExit('Exit because mouse moved')
 
+    def clicker_process(self):
+        self.log.debug('Clicker start')
+        x_offset = 0  # 1920x1080
+        # x_offset = 1920  # 3840x1080
+        clean_position = (215, 150)
+        if self.is_fullscreen:
+            clean_position = (23, 23)
+        self.log.debug('Activate game')
+        bottle_buy_price = 85
+        bottle_sell_price = 90
+        sell_profit = bottle_sell_price - bottle_buy_price
+        cash_value = self.cash_value
+        self.log.debug('Initial cash value {}'.format(cash_value))
+        if cash_value < bottle_buy_price:
+            raise SystemExit(
+                'Cash value less than poision bottle price {}'.format(bottle_buy_price))
+        self.mouse_click(*clean_position, delay=0.7, x_offset=x_offset)
+        while True:
+            if self.get_mouse_position() == (0, 0):
+                raise SystemExit('Exit because mouse position (0, 0)')
+            bottle_number = cash_value // bottle_buy_price
+            self.log.debug('Poision bottles number {}'.format(bottle_number))
+            cash_value += bottle_number * sell_profit
+            self.log.debug('New cash value {}'.format(cash_value))
+            self.hero_of_the_kingdom_trade_iteration(bottle_number)
+        self.log.debug('Clicker finish')
 
-def mouse_click(x, y, x_offset=0, y_offset=0, delay=0.0):
-    mouse = Controller()
-    mouse.position = (x + x_offset, y + y_offset)
-    print('click', mouse.position)
-    time.sleep(0.1)
-    mouse.press(Button.left)
-    time.sleep(0.1)
-    mouse.release(Button.left)
-    time.sleep(delay)
-    if get_mouse_position() != (x + x_offset, y + y_offset):
-        print('Exit because mouse moved')
-        exit(1)
-
-
-def clicker_process():
-    print('clicker start')
-    x_offset = 0  # 1920x1080
-    # x_offset = 1920  # 3840x1080
-    # clean_position = (23, 23)  # fullscreen
-    clean_position = (215, 150)
-    print('Activate game')
-    mouse_click(*clean_position, delay=0.7, x_offset=x_offset)
-    bottle_buy_price = 85
-    bottle_sell_price = 90
-    cash_value = 5085
-    print('initial cash value', cash_value)
-    while True:
-        if get_mouse_position() == (0, 0):
-            print('Exit because mouse position (0, 0)')
-            break
-        bottle_number = cash_value // bottle_buy_price
-        print('bottle_number', bottle_number)
-        cash_value = cash_value + bottle_number * \
-            (bottle_sell_price - bottle_buy_price)
-        print('new cash value', cash_value)
-        hero_of_the_kingdom_trade_iteration(bottle_number)
-    print('clicker finish')
-
-
-def hero_of_the_kingdom_trade_iteration(bottle_number):
-    print('Trade iteration start')
-    x_offset = 0  # 1920x1080
-    # x_offset = 1920  # 3840x1080
-    if False:
-        map_position = (75, 75)  # fullscreen
-        map_city_position = (410, 450)  # fullscreen
-        map_secret_glade_position = (1310, 145)  # fullscreen
-        bootlegger_position = (1595, 220)  # fullscreen
-        bootlegger_buy_bottle_position = (1535, 340)  # fullscreen
-        bootlegger_trade_confirm_position = (1550, 600)  # fullscreen
-        hunter_position = (485, 495)  # fullscreen
-        hunter_sell_bottle_position = (675, 465)  # fullscreen
-        hunter_trade_confirm_position = (430, 735)  # fullscreen
-    else:
-        map_position = (250, 195)
-        map_city_position = (525, 490)
-        map_secret_glade_position = (1240, 255)
-        bootlegger_position = (1470, 305)
-        bootlegger_buy_bottle_position = (1415, 400)
-        bootlegger_trade_confirm_position = (1430, 620)
-        hunter_position = (580, 525)
-        hunter_sell_bottle_position = (735, 505)
-        hunter_trade_confirm_position = (535, 720)
-    print('Open map')
-    mouse_click(*map_position, delay=2, x_offset=x_offset)
-    print('Travel to city')
-    mouse_click(*map_city_position, delay=1.7, x_offset=x_offset)
-    print('Trade with bootlegger')
-    mouse_click(*bootlegger_position, delay=1.7, x_offset=x_offset)
-    print('Buy {} poision bottles'.format(bottle_number))
-    for i in range(bottle_number):
-        mouse_click(*bootlegger_buy_bottle_position, delay=0.5, x_offset=x_offset)
-    print('Confirm trade')
-    mouse_click(*bootlegger_trade_confirm_position, delay=0.7, x_offset=x_offset)
-    print('Open map')
-    mouse_click(*map_position, delay=2, x_offset=x_offset)
-    print('Travel to glade')
-    mouse_click(*map_secret_glade_position, delay=1.7, x_offset=x_offset)
-    print('Trade with hunter')
-    mouse_click(*hunter_position, delay=1.7, x_offset=x_offset)
-    print('Sell {} poision bottles'.format(bottle_number))
-    for i in range(bottle_number):
-        mouse_click(*hunter_sell_bottle_position, delay=0.5, x_offset=x_offset)
-    print('Confirm trade')
-    mouse_click(*hunter_trade_confirm_position, delay=0.7, x_offset=x_offset)
-    print('Trade iteration finish')
+    def hero_of_the_kingdom_trade_iteration(self, bottle_number):
+        self.log.debug('Trade iteration start')
+        x_offset = 0  # 1920x1080
+        # x_offset = 1920  # 3840x1080
+        if self.is_fullscreen:
+            map_position = (75, 75)
+            map_city_position = (410, 450)
+            map_secret_glade_position = (1310, 145)
+            bootlegger_position = (1595, 220)
+            bootlegger_buy_bottle_position = (1535, 340)
+            bootlegger_trade_confirm_position = (1550, 600)
+            hunter_position = (485, 495)
+            hunter_sell_bottle_position = (675, 465)
+            hunter_trade_confirm_position = (430, 735)
+        else:
+            map_position = (250, 195)
+            map_city_position = (525, 490)
+            map_secret_glade_position = (1240, 255)
+            bootlegger_position = (1470, 305)
+            bootlegger_buy_bottle_position = (1415, 400)
+            bootlegger_trade_confirm_position = (1430, 620)
+            hunter_position = (580, 525)
+            hunter_sell_bottle_position = (735, 505)
+            hunter_trade_confirm_position = (535, 720)
+        self.log.debug('Open map')
+        self.mouse_click(*map_position, delay=2, x_offset=x_offset)
+        self.log.debug('Travel to city')
+        self.mouse_click(*map_city_position, delay=1.7, x_offset=x_offset)
+        self.log.debug('Trade with bootlegger')
+        self.mouse_click(*bootlegger_position, delay=1.7, x_offset=x_offset)
+        self.log.debug('Buy {} poision bottles'.format(bottle_number))
+        for i in range(bottle_number):
+            self.mouse_click(*bootlegger_buy_bottle_position,
+                             delay=0.5, x_offset=x_offset)
+        self.log.debug('Confirm trade')
+        self.mouse_click(*bootlegger_trade_confirm_position,
+                         delay=0.7, x_offset=x_offset)
+        self.log.debug('Open map')
+        self.mouse_click(*map_position, delay=2, x_offset=x_offset)
+        self.log.debug('Travel to glade')
+        self.mouse_click(*map_secret_glade_position,
+                         delay=1.7, x_offset=x_offset)
+        self.log.debug('Trade with hunter')
+        self.mouse_click(*hunter_position, delay=1.7, x_offset=x_offset)
+        self.log.debug('Sell {} poision bottles'.format(bottle_number))
+        for i in range(bottle_number):
+            self.mouse_click(*hunter_sell_bottle_position,
+                             delay=0.5, x_offset=x_offset)
+        self.log.debug('Confirm trade')
+        self.mouse_click(*hunter_trade_confirm_position,
+                         delay=0.7, x_offset=x_offset)
+        self.log.debug('Trade iteration finish')
 
 
 class ClickerWindow(Gtk.Window):
@@ -128,7 +133,8 @@ class ClickerWindow(Gtk.Window):
         Gtk.Window.__init__(self, title='Hero of the Kingdom Money')
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
 
-        adjustment = Gtk.Adjustment(0, 0, 999999, 1, 10, 0)
+        adjustment = Gtk.Adjustment(value=0, lower=0, upper=999999,
+                                    step_increment=1, page_increment=10, page_size=0)
         self.money = Gtk.SpinButton()
         self.money.set_adjustment(adjustment)
         self.money.set_numeric(True)
@@ -151,7 +157,9 @@ class ClickerWindow(Gtk.Window):
         self.progressbar_value(value=0)
         self.progressbar.set_text('Money mining')
         self.progressbar.set_show_text(True)
-        get_mouse_position(output=True)
+        cash_value = int(self.money.get_text())
+        clicker_exec = ClickerExecutor(cash_value=cash_value)
+        clicker_exec.clicker_process()
 
 
 win = ClickerWindow()
